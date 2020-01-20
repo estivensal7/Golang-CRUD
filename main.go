f256096a57491bb9ad8a837a6dfb354c9c880c45
estivensal7 <estivensal7@gmail.com>
2020-01-20 15:38:45 -0500

    implemented server, and established CRUD routes. GET Books is done. Implementing GET Book, CREATE Book, UPDATE Book, and DELETE Book next.

--- main.go
+++ main.go
@@ -1,13 +1,70 @@
 package main
 
+//imported packages
 import (
 	"encoding/json"
-	"fmt"
+	// "fmt"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 )
 
+//book model
+type Book struct {
+	ID int `json:id`
+	Title string `json:title`
+	Author string `json:author`
+	Year string `json:year`
+}
+
+//the book slice will hold the book record that we are going to create
+var books []Book
+
 func main() {
+
+	//https://github.com/gorilla/mux#install
+	//implementing the mux request router
+	router := mux.NewRouter()
+
+	//appending data to the books slice
+	books = append(books,
+		Book{ID: 1, Title: "Golang pointers", Author: "Mr. Golang", Year: "2010"},
+		Book{ID: 2, Title: "Goroutines", Author: "Mr. Goroutine", Year: "2011"},
+		Book{ID: 3, Title: "Golang routers", Author: "Mr. Router", Year: "2012"},
+		Book{ID: 4, Title: "Golang concurrency", Author: "Mr. Currency", Year: "2013"},
+		Book{ID: 5, Title: "Golang good parts", Author: "Mr. Good", Year: "2014"})
 	
+	//creating routes for CRUD capabilities
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+
+	//starting our server
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
+
+//the getBooks func takes two parameters ---
+//'w http.ResponseWriter' is used to fill in the HTTP response
+//'r *http.Request' holds the request object
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get Books is called")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get Book is called")
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Add Book is called")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Update Book is called")
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Delete Book is called")
 }
